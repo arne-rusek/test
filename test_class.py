ce713e7aae7f3c2ccc3e417dd6f92df4ada3f541
Arne Rusek <arne.rusek@mayadata.io>
2020-12-31 16:03:12 +0100

    add test_three

--- test_class.py
+++ test_class.py
@@ -3,6 +3,11 @@ class TestClass:
         """Documentation of test_one"""
         x = "this"
         assert 'h' in x
+
     def test_two(self):
         x = "hello"
         assert hasattr(x, 'bang_zoom')
+
+    def test_three(self):
+        x = 'shorty'
+        assert x.find('longy') != -1
